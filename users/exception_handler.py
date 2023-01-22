e89959f595b2ab2f9b909e95f4584d0e57c27c98
PrantaChakraborty <cbpranta@gmail.com>
2023-01-22 21:20:07 +0600

    update exception_handler.py

--- users/exception_handler.py
+++ users/exception_handler.py
@@ -34,10 +34,21 @@ def custom_exception_handler(exc, context):
     # Call REST framework's default exception handler first,
     # to get the standard error response.
     response = exception_handler(exc, context)
-    logger.info(f"{exc}")
     if response is not None:
-        response.data['success'] = False
-        response.data["error"] = str(exc.detail)
-        del response.data['detail']
+        if response.status_code == 404:
+            response.data['success'] = False
+            response.data["error"] = str(exc)
+            del response.data['detail']
+        elif response.status_code == 500:
+            response.data['success'] = False
+            response.data["error"] = "Internal server error"
+        elif response.status_code == 400:
+            response.data['success'] = False
+            response.data["error"] = "Bad request"
+            logger.info(f"{response.data}")
+        else:
+            response.data['success'] = False
+            response.data["error"] = str(exc.detail)
+            del response.data['detail']
     return response
 
